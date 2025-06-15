7ae24ba17ad0c289fcab58ae0d9715d91ab60465
SupernovaXTS <dylanhermangamer@gmail.com>
2025-06-15 18:46:17 -0400

    part 3

--- game_map.py
+++ game_map.py
@@ -7,7 +7,7 @@ import tile_types
 class GameMap:
     def __init__(self, width: int, height: int):
         self.width, self.height = width, height
-        self.tiles = np.full((width, height), fill_value=tile_types.floor, order="F")
+        self.tiles = np.full((width, height), fill_value=tile_types.wall, order="F")
 
         self.tiles[30:33, 22] = tile_types.wall
 
@@ -16,4 +16,4 @@ class GameMap:
         return 0 <= x < self.width and 0 <= y < self.height
 
     def render(self, console: Console) -> None:
-        console.tiles_rgb[0:self.width, 0:self.height] = self.tiles["dark"]
+        console.rgb[0:self.width, 0:self.height] = self.tiles["dark"]
