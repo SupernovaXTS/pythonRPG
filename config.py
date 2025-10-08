1260af1b3c7fd66c8e694ce0020321deb4558ba8
SupernovaXTS <dylanhermangamer@gmail.com>
2025-10-08 12:09:51 -0400

    add arrow movement direction

--- config.py
+++ config.py
@@ -3,9 +3,11 @@ class config():
     # Unused modular config
     def __init__(self):
         self.keys = types.SimpleNamespace()
+        keys = self.keys
         self.keys.movement = types.SimpleNamespace()
-        self.keys.movement.wasd = types.SimpleNamespace()
-        wasd = self.keys.movement.wasd
+        keys.movement.wasd = types.SimpleNamespace()
+        
+        wasd = keys.movement.wasd
         wasd.north = 'w'
         wasd.south = 's'
         wasd.west = 'a'
@@ -14,8 +16,8 @@ class config():
         wasd.northeast = 'e'
         wasd.southwest = 'z'
         wasd.southeast = 'c'
-        self.keys.movement.numpad = types.SimpleNamespace()
-        numpad = self.keys.movement.numpad
+        keys.movement.numpad = types.SimpleNamespace()
+        numpad = keys.movement.numpad
         numpad.north = '8'
         numpad.south = '2'
         numpad.west = '4'
@@ -24,3 +26,13 @@ class config():
         numpad.northeast = '9'
         numpad.southwest = '1'
         numpad.southeast = '3'
+        keys.movement.arrows = types.SimpleNamespace()
+        arrows = keys.movement.arrows
+        arrows.north = 'up'
+        arrows.south = 'down'
+        arrows.west = 'left'
+        arrows.east = 'right'
+        arrows.northwest = 'home'
+        arrows.northeast = 'pageup'
+        arrows.southwest = 'end'
+        arrows.southeast = 'pagedown'
