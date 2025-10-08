5b0736a59b1314c6fd7718dc544e1991a4536ad1
SupernovaXTS <dylanhermangamer@gmail.com>
2025-10-08 12:06:38 -0400

    Rename config directions to compass directions

--- config.py
+++ config.py
@@ -6,10 +6,21 @@ class config():
         self.keys.movement = types.SimpleNamespace()
         self.keys.movement.wasd = types.SimpleNamespace()
         wasd = self.keys.movement.wasd
-        wasd.forward = 'w'
-        wasd.back = 's'
-        wasd.left = 'a'
-        wasd.right = 'd'
+        wasd.north = 'w'
+        wasd.south = 's'
+        wasd.west = 'a'
+        wasd.east = 'd'
+        wasd.northwest = 'q'
+        wasd.northeast = 'e'
+        wasd.southwest = 'z'
+        wasd.southeast = 'c'
         self.keys.movement.numpad = types.SimpleNamespace()
         numpad = self.keys.movement.numpad
-        
+        numpad.north = '8'
+        numpad.south = '2'
+        numpad.west = '4'
+        numpad.east = '6'
+        numpad.northwest = '7'
+        numpad.northeast = '9'
+        numpad.southwest = '1'
+        numpad.southeast = '3'
